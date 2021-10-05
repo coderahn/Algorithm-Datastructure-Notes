f0c10ecde65624f5a60ef743f184885004a9efef
ash2748 <max3535@naver.com>
2021-10-05 23:26:59 +0900

    동빈북PART2 - 그리디

---
+++ 파이썬 기초 문법 정리/PART2-그리디.py
@@ -0,0 +1,87 @@
+###그리디###
+
+#<내 풀이>
+##2.큰수의법칙(동빈북 92P)
+#입력
+n,m,k = map(int, input().split())
+arr = list(map(int,input().split()))
+
+#정렬
+arr.sort(reverse=True)
+
+#로직 작성
+sum = 0
+cnt = 0
+pointer = 0
+for i in range(m):
+  sum+=arr[pointer]
+  cnt+=1
+
+  #포인터가 한번 증가한 상태인경우
+  if pointer != 0:
+    #증가한 포인터와 첫번째 포인터 값이 다른 경우(뒤에가 무조건 작은 경우)
+    if arr[pointer] != arr[pointer - 1]:
+      #포인터를 초기화(가장 큰 수를 더하기)
+      pointer = 0
+      cnt = 0
+
+  if (cnt == k):
+    pointer += 1
+    cnt = 0
+else:
+  print(sum)
+
+#arr[0]을 sum에 더하면서 cnt를 증가시킨후, cnt가 k와 같을 때 arr[1]을 위한 pointer를 증가시키고, cnt를 0으로 초기화
+#arr[1]을 더하고난 후, 바로 가장 최대값인 arr[0]으로 더하기 위해 그 작업을 중간에 해준다. 
+#arr[0]과 arr[1]이 같으면, 바로 arr[0]으로 돌아갈 수 있게 pointer를 0으로 초기화하고 cnt도 0으로 초기화한다.
+#if arr[pointer] != arr[pointer - 1]: -> 이조건이 없어도 결과는 같은듯 하다. [0]과 [1]이 같으면 그냥 앞에꺼를 더하든 두번째꺼를 더하든 같기 때문에 초기화만 해주면 어떤 경우도 포함한다.
+
+#<동빈북 풀이1>
+#이중 반복문이지만 절차지향으로 가독성 좋게 푼 것 같다.
+n,m,k = map(int,input().split())
+data = list(map(int,input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+
+while True:
+  #최대값더하기 반복
+  for i in range(k):
+    if m == 0:
+      break
+    result += first
+    m-=1
+  if m == 0:
+    break
+  result += second
+  m-=1
+  
+print(result)
+
+#<동빈북 풀이2>
+#갓갓..반복되는 패턴이 있음. 6,6,6,5   6,6,6,5 ...
+#6,6,6,5로 반복되는 수열
+#k+1은 반복되는 수열의 길이(4)
+#m을 k+1로 나누면 반복되는 수열의 횟수(2)
+#나눌 때 꼭 짝수로 떨어지지 않을 수 있음. 나머지연산자로 +1을 해줄 수도 있음
+#이 횟수에 k를 곱하면 가장 큰 수의 반복 갯수(6)
+#가장큰값을 반복갯수(6)으로 곱하고, 나머지는 second를 더하면됨
+
+n,m,k=map(int,input().split())
+data = list(map(int,input().split()))
+
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
+
+count = int(m / (k + 1))
+count += m % (k + 1)
+
+result = 0
+result += count * first 
+result += (m-count) * second
+
+print(result)

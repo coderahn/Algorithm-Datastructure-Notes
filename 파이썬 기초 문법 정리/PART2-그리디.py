23db456d62c94fb44c12bc9755afd89d9e87765e
ash2748 <max3535@naver.com>
2021-10-11 21:26:21 +0900

    no message

--- 파이썬 기초 문법 정리/PART2-그리디.py
+++ 파이썬 기초 문법 정리/PART2-그리디.py
@@ -185,3 +185,113 @@ while True:
 
 result += (n-1)
 print(result)
+
+##5.게임 개발(동빈북118p)
+
+#<내 풀이>
+n,m = map(int,input().split())
+A,B,d = map(int, input().split())
+mapArr = []
+
+#맵생성
+for _ in range(n):
+  mapArr.append(list(map(int,input().split())))
+
+dArr = [(-1,0),(0,-1),(1,0),(0,1)]
+
+rightCnt = 1
+cnt = 0
+
+while True:
+  d=d+1
+
+  if d > 3:
+    d = 0
+  
+  xy = dArr[d]
+
+  nextA = A + int(xy[0])
+  nextB = B + int(xy[1])
+
+  val = mapArr[nextA][nextB]
+
+  if val == 0:
+    rightCnt = rightCnt + 1
+    A,B = nextA,nextB
+  else:
+    cnt = cnt + 1
+    mapArr[nextA][nextB] = 2
+
+  if cnt == 4:
+    break
+
+
+for val in mapArr:
+  for val2 in val:
+    print(val2, end=' ')
+  print()
+
+print(rightCnt)
+
+#<동빈북 풀이>
+n, m = map(int, input().split())
+
+#방문 위치 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+
+#현재캐릭터의 X좌표, Y좌표, 방향
+x, y, direction = map(int, input().split())
+d[x][y] = 1
+
+#전체맵정보
+array = []
+for i in range(n):
+  array.append(list(map(int, input().split())))
+
+#북, 동, 남, 서
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+#왼쪽으로 회전
+def turn_left():
+  global direction #direction이 함수 바깥에 선언되었기 때문에 global 선언하여 접근
+  direction -= 1
+  if direction == -1:
+    direction = 3
+
+#시뮬레이션 시작
+count = 1
+turn_time = 0
+while True:
+  #왼쪽으로 회전
+  turn_left()
+  nx = x + dx[direction]
+  ny = y + dy[direction]
+
+  #회전 후 정면에 가보지 않은 칸이 존재하는 경우 이동
+  if d[nx][ny] == 0 and array[nx][ny] == 0:
+    d[nx][ny] = 1
+    x = nx
+    y = ny
+    count += 1
+    turn_time = 0
+    continue
+  #회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+  else:
+    turn_time += 1
+  
+  #네 방향 모두 갈 수 없는 경우
+  if turn_time == 4:
+    nx = x - dx[direction]
+    ny = y - dy[direction]
+    #뒤로 갈 수 있다면 이동하기
+    if array[nx][ny] == 0:
+      x = nx
+      y = ny
+    #뒤가 바다로 막혀있는 경우
+    else:
+      break
+    trun_time = 0
+  
+print(count)
+

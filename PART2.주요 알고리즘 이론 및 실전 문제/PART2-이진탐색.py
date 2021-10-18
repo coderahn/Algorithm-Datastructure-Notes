95cc52196e2fe111039b0020d0b4bf371ee7eca9
ash2748 <max3535@naver.com>
2021-10-18 21:29:04 +0900

    이진탐색 문제 1개(부품찾기)

--- PART2.주요 알고리즘 이론 및 실전 문제/PART2-이진탐색.py
+++ PART2.주요 알고리즘 이론 및 실전 문제/PART2-이진탐색.py
@@ -48,3 +48,50 @@ if result == None:
 else:
   print(result + 1)
 
+##부품찾기
+
+'''
+정렬을 위한 시간복잡도 O(N * logN)
+이진탐색을 위한 시간복잡도 O(M * logN)
+총 시간복잡도 O((N+M) * logN)
+'''
+
+#내풀이(동빈북 197p)
+n = int(input())
+arr1 = list(map(int, input().split()))
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+arr1.sort()
+
+def binarySearch(arr, target, start, end):
+  if start > end:
+    return 0
+  
+  mid = (start + end) // 2
+
+  if arr[mid] == target:
+    return 1
+  elif arr[mid] > target:
+    return binarySearch(arr, target, start, mid-1)
+  elif arr[mid] < target:
+    return binarySearch(arr, target, mid+1, end) 
+  else:
+    return 0
+
+for i in range(m):
+  if 1== binarySearch(arr1, arr2[i], 0, n-1):
+    print('yes', end= ' ')
+  else:
+    print('no', end= ' ')
+
+'''
+입력
+5
+8 3 7 9 2
+3
+5 7 9
+출력
+no yes yes
+'''

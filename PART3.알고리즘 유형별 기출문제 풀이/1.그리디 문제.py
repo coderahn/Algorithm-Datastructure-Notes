9dd6eaa6f4714b1e951394aad6538f1982a5a29e
ash2748 <max3535@naver.com>
2021-11-26 06:53:59 +0900

    기출실전-그리디문제-곱하기 혹은 더하기

--- PART3.알고리즘 유형별 기출문제 풀이/1.그리디 문제.py
+++ PART3.알고리즘 유형별 기출문제 풀이/1.그리디 문제.py
@@ -2,6 +2,10 @@
 
 ##01.모험가 길드
 
+'''
+-굳이 deque를 썼는데 쉽게 푸는 연습을 하자
+'''
+
 #내풀이
 from collections import deque
 
@@ -33,4 +37,31 @@ print(result)
 2 3 1 2 2 
 출력
 2
+'''
+
+##02.곱하기 혹은 더하기
+
+'''
+-주어진 입력 값 숫자 사이사이 + 또는 *을 이용해 가장 큰 수 만들기
+-0인 경우에만 더하고 계속 곱하면 됨
+'''
+
+#내풀이
+arr = list(map(int, input()))
+arr.sort(reverse=True)
+result = arr[0]
+
+for i in range(1, len(arr)):
+  if result * arr[i] == 0:
+    result += arr[i]
+  else:
+    result *= arr[i]
+
+print(result)
+
+'''
+입력
+02984
+출력
+567
 '''
